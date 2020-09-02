00b4642ff5817efc167def6d7eaea1d8b22b50e7
Florian GOZARD <florian.gzd92@gmail.com>
2020-09-02 18:19:05 +0200

    flo - fix pr les gd
    affiche une erreur qd des valeurs sont mauvaises entre elles

--- EBNE/Assets/Florian/Scripts/TreeController.cs
+++ EBNE/Assets/Florian/Scripts/TreeController.cs
@@ -10,8 +10,11 @@ public class TreeController : MonoBehaviour
     [Tooltip("Le temps avant de désactiver l'arbre afin de gagner en performance (dès que le joueur change d'arbre idéalement)")]
     public int timeAlive;
 
+    private float _oc;
+
     void Start()
     {
+        _oc = FindObjectOfType<ObstacleController>().fallingSpeed;
         StartCoroutine(CheckSpeedValues());
         StartCoroutine(DesactivateTheTree());
     }
@@ -24,8 +27,8 @@ public class TreeController : MonoBehaviour
     IEnumerator CheckSpeedValues()
     {
         yield return new WaitForSeconds(3.5f);
-        if (treeSpeed != FindObjectOfType<ObstacleController>().fallingSpeed) Debug.LogError("The fall value (" + FindObjectOfType<ObstacleController>().fallingSpeed + ")" +
-             " isn't equal to the treeSpeed (" + treeSpeed + "). Set the fall value to " + treeSpeed + " or the tree speed to " + FindObjectOfType<ObstacleController>().fallingSpeed + ".");
+        if (treeSpeed != _oc) Debug.LogError("The fall value (" + _oc + ")" +
+             " isn't equal to the treeSpeed (" + treeSpeed + "). Set the fall value to " + treeSpeed + " or the tree speed to " + _oc + ".");
     }
 
     IEnumerator DesactivateTheTree()
